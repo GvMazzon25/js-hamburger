92109ea30a009c9aaf94778c7de6754d19962679
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-22 16:46:47 +0200

    first-click

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Element Selection
+const buttonHamb = document.querySelector('.hamburger-menu');
+const fas = document.querySelector('.fas')
+const close = document.querySelector('')
+
+fas.addEventListener('click', function(){
+   buttonHamb.classList.add('d-block')
+})
