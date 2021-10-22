bee7f360098f8870cc3a149e636eaf6e4021f91f
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-22 16:48:58 +0200

    style-display

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 //Element Selection
 const buttonHamb = document.querySelector('.hamburger-menu');
 const fas = document.querySelector('.fas')
-const close = document.querySelector('')
 
 fas.addEventListener('click', function(){
-   buttonHamb.classList.add('d-block')
+   //buttonHamb.classList.add('d-block');
+   buttonHamb.style.display = 'block';
 })
